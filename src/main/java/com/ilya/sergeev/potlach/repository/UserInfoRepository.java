6e330cf905b2045c051b2e9693f37c06b029e7b0
hensh <master.hensh@gmail.com>
2014-11-17 17:20:30 +0300

    refactoring

--- src/main/java/com/ilya/sergeev/potlach/repository/UserInfoRepository.java
+++ src/main/java/com/ilya/sergeev/potlach/repository/UserInfoRepository.java
@@ -14,6 +14,7 @@ public interface UserInfoRepository  extends CrudRepository<UserInfo, Long>
 {
 	public UserInfo findByName(String name);
 	
+	//return top rate users
 	@Query("select u from UserInfo u order by u.rating DESC")
 	public Collection<UserInfo> getTopRate();
 }
